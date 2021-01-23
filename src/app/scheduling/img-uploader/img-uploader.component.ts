a2eb3dff385caa8b5750b3270cf6b14774806186
diegodanielfranca <diegodaniel.f@hotmail.com>
2021-01-23 15:24:31 -0300

    ajuste label de update de imagem e upload

--- src/app/scheduling/img-uploader/img-uploader.component.ts
+++ src/app/scheduling/img-uploader/img-uploader.component.ts
@@ -38,10 +38,11 @@ export class ImgUploaderComponent implements OnInit {
         return false;
       }
 
-      if (![].includes(allowed_types, fileInput.target.files[0].type)) {
-        this.imageError = 'Only Images are allowed ( JPG | PNG )';
-        return false;
-      }
+      // if (![].includes(allowed_types, fileInput.target.files[0].type)) {
+      //   this.imageError = 'Only Images are allowed ( JPG | PNG )';
+      //   return false;
+      // }
+      
       const reader = new FileReader();
       reader.onload = (e: any) => {
         const image = new Image();
